431e940803ebbe30a7a0fc5f742a6d63ded357b4
Luke <pilot@Luke.local>
2023-01-02 16:16:38 +0200

    found typo in file where database pool is created

--- server/utils/database.js
+++ server/utils/database.js
@@ -4,7 +4,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 const pool = new POOL({
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
