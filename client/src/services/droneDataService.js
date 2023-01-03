b913019e266995dde0fad30607fa7a0f5fe8a4f2
Luke <lukelonnroth@windowslive.com>
2023-01-03 18:19:07 +0200

    Fine tuning frontend, icon sizes and changing words.

--- client/src/services/droneDataService.js
+++ client/src/services/droneDataService.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:5000";
-//const API_URL = "https://reaktor-nesty.herokuapp.com"
+const API_URL = "https://reaktor-nesty.herokuapp.com"
 
 export const droneDataService = async () => {
 	try {
