80fd4cf19e180d0aaff674fa938d4b10608c21dc
Luke <lukelonnroth@windowslive.com>
2023-01-03 18:34:30 +0200

    Removed CSS class that is not used

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ const App = () => {
 	return (
 		<>
 			{violatorPilots.length === 0 ? (<></>) :
-				(<div className='app'>
+				(<div>
 					<BackgroundImage/>
 					<Header/>
 					<ClosestViolator violator={closestViolator}/>
