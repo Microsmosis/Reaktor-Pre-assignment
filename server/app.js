c239a880d64c16e2ca0b9dc63b7787155e074530
Luke <lukelonnroth@windowslive.com>
2023-01-03 18:23:02 +0200

    Removed useless middleware.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 const droneRouter = require("./controllers/droneRouter");
 
 app.use(
@@ -8,8 +7,6 @@ app.use(
     origin: "https://reaktor-nesty.herokuapp.com",
   })
 );
-
-app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
 	const path = require('path');
