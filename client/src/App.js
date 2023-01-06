2a2e53488f48bca70ef386d7d40a212a1d71292e
Luke <lukelonnroth@windowslive.com>
2023-01-06 11:20:18 +0200

    Added services to call internal API's if socket connection is not made or some kind of error with sockets

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import io from "socket.io-client";
 import { useState } from 'react';
 import { List } from './components/List';
 import { Header } from './components/Header';
+import { distanceService } from './services/closestDistanceService';
+import { pilotDataService } from './services/pilotDataService';
 import { BackgroundImage } from './components/BacgroundImage';
 import { ClosestDistance } from './components/ClosestDistance';
 import { useInterval } from './utils/useInterval';
@@ -14,16 +16,29 @@ const App = () => {
 	const [closestDistance, setClosestDistance] = useState(0);
 
 	useInterval(() => {
-		socket?.emit("violators", (violators) => {
-			if(violators?.length) {
-				setViolatorPilots(violators);
-			}
-		});
-		socket?.emit("closest_distance", (distance) => {
-			if(distance) {
-				setClosestDistance(distance);
-			}
-		});
+		if(socket) {
+			socket.emit("violators", (violators) => {
+				if(violators?.length) {
+					setViolatorPilots(violators);
+				}
+			});
+			socket.emit("closest_distance", (distance) => {
+				if(distance) {
+					setClosestDistance(distance);
+				}
+			});
+		} else {
+			pilotDataService().then((violators) => {
+				if(violators?.length) {
+					setViolatorPilots(violators);
+				}
+			});
+			distanceService().then((distance) => {
+				if(distance) {
+					setClosestDistance(distance);
+				}
+			});
+		}
 	}, 2000);
 
 	return (
